07a09aebe5b55b89c0cef573225c9aa43f3110b2
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-08-18 01:17:01 +0530

    addnewtask coponent add addnewrask display function add

--- src/component/home.js
+++ src/component/home.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
+import AddNewTask from "./addNewTask";
 import TodoItem from "./todoItem";
+
 const Home = () => {
+  const [show, setShow] = useState(false);
+  const tasks = [
+    { id: 10, task: "sample" },
+    { id: 10, task: "sample" },
+    { id: 10, task: "sample" },
+  ];
   return (
     <>
       <h1>todo</h1>
-      <button>Add new Task</button>
-      <TodoItem />
+      <button onClick={() => setShow(!show)}>Add new Task</button>
+      {tasks.map((task) => (
+        <TodoItem />
+      ))}
+      {show && <AddNewTask />}
     </>
   );
 };
