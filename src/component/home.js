2fdcd916669cc6b82e85d6d471635ac833bc28b8
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-08-17 23:49:05 +0530

    add home page

---
+++ src/component/home.js
@@ -0,0 +1,11 @@
+import TodoItem from "./todoItem";
+const Home = () => {
+  return (
+    <>
+      <h1>todo</h1>
+      <button>Add new Task</button>
+      <TodoItem />
+    </>
+  );
+};
+export default Home;
