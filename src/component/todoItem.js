26307fa4317bd17cf1107466d3298272aa889894
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-08-19 03:06:40 +0530

    functionality add -onCompleted function -remove reducer add

--- src/component/todoItem.js
+++ src/component/todoItem.js
@@ -1,14 +1,41 @@
-import { useState } from "react";
+import { useContext } from "react";
+import TodoContext from "../store/todoContext";
 import classes from "./todoItem.module.css";
 
-const TodoItem = () => {
-  const [completed, setCompleted] = useState(true);
+const TodoItem = ({ content }) => {
+  const todoContext = useContext(TodoContext);
+
+  const onCompleted = (taskId) => {
+    console.log("task", taskId);
+    todoContext.todoDispatch({
+      type: "UPDATE",
+      id: taskId,
+    });
+  };
+
   return (
     <div>
       <div className={classes.todoBoxSection}>
-        <input type="checkbox" id="status" name="status" />
-        <h5 className={completed == true ? classes.completedTask : ""}>Task</h5>
-        <button>Delete</button>
+        <input
+          type="checkbox"
+          id="status"
+          name="status"
+          onChange={() => {
+            onCompleted(content.id);
+          }}
+        />
+        <h5
+          className={content.isCompleted === true ? classes.completedTask : ""}
+        >
+          {content.name}
+        </h5>
+        <button
+          onClick={() => {
+            todoContext.todoDispatch({ type: "REMOVE", id: content.id });
+          }}
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
