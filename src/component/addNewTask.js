01497420d91e12dddd109be42a0da99180ffc1ac
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-08-19 03:08:29 +0530

    new task add function add text input handle impelement

--- src/component/addNewTask.js
+++ src/component/addNewTask.js
@@ -1,18 +1,42 @@
+import { useContext, useState } from "react";
+import TodoContext from "../store/todoContext";
 import { Modal, Button } from "react-bootstrap";
 
-const AddNewTask = () => {
+const AddNewTask = ({show}) => {
+  const [newTask, setNewTask] = useState();
+  const ctx = useContext(TodoContext);
+
+  const addNewTaskHandler = (event) => {
+    event.preventDefault();
+    console.log(event.target.value);
+    setNewTask(event.target.value);
+  };
+
+  const submit = () => {
+    ctx.todoDispatch({
+      type: "ADD",
+      task: {
+        id: Math.floor(Math.random() * 6) + 1,
+        name: newTask,
+        isCompleted: false,
+      },
+    });
+  };
+
   return (
-    <Modal show={true}>
-      <Modal.Header closeButton>
+    <Modal show={show}>
+      <Modal.Header >
         <Modal.Title>Add New Item</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <label>Title</label>
-        <input type="text" id="task" name="task" />
+        <input type="text" id="task" name="task" onChange={addNewTaskHandler} />
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary">Cancel</Button>
-        <Button variant="primary">Confirm</Button>
+        <Button variant="primary" onClick={submit}>
+          Confirm
+        </Button>
       </Modal.Footer>
     </Modal>
   );
