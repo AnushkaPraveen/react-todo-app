43e4b8ef0be27ae01385f9660053986fb1de4823
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-08-19 23:59:53 +0530

    modal context handle add no data message add

--- src/component/home.js
+++ src/component/home.js
@@ -1,18 +1,24 @@
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import AddNewTask from "./addNewTask";
 import TodoItem from "./todoItem";
 import TodoContext from "../store/todoContext";
-import { Modal, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 const Home = () => {
-  const [show, setShow] = useState(false);
   const taskContext = useContext(TodoContext);
+  const handleModal = () => {
+    taskContext.todoDispatch({
+      type: "MODALSHOW",
+    });
+  };
 
   return (
     <>
       <h1>todo</h1>
-      <AddNewTask show={show} />
-      <Button onClick={() => setShow(!show)}>Add new Task</Button>
+      <AddNewTask />
+      <Button onClick={handleModal}>Add new Task</Button>
+      <h5>Todo List</h5>
+      {taskContext.items.length < 1 ? <p>No Data</p> : ""}
       {taskContext.items.map((task) => (
         <TodoItem key={task.id} content={task} />
       ))}
