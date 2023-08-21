75d56a5b831f4d1309d2cf6258f734a988fd1e44
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-08-22 01:27:49 +0530

    add dispatch functions useEffect add

--- src/store/todoContext.js
+++ src/store/todoContext.js
@@ -1,4 +1,5 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
+import { GetAllTasks } from "../api/todoApi";
 
 const TodoContext = React.createContext({
   tasks: "sample",
@@ -50,6 +51,40 @@ export const TodoContextProvider = (props) => {
     todoReducer,
     defaultTodoState
   );
+  useEffect(() => {
+    setLoadingStatus(true);
+    const fetchData = async () => {
+      try {
+        const result = await GetAllTasks();
+        const tasksArray = result.items; // Assuming result.items is an array of tasks
+        const filteredNewTasks = tasksArray.filter((newTask) =>
+          todoState.tasks.every(
+            (existingTask) => existingTask.id !== newTask.id
+          )
+        );
+
+        filteredNewTasks.map((task) => setTasks(task));
+
+        setLoadingStatus(false);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      }
+    };
+
+    fetchData();
+  }, [todoState.tasks]);
+
+  const setTasks = (payload) => {
+    dispatchTodoAction({ type: "ADD", task: payload });
+  };
+
+  const removeTask = (id) => {
+    dispatchTodoAction({ type: "REMOVE", id: id });
+  };
+
+  const updateTask = (id) => {
+    dispatchTodoAction({ type: "UPDATE", id: id });
+  };
 
   const setLoadingStatus = (status) => {
     dispatchTodoAction({ type: "LOADING", status: status });
@@ -66,6 +101,9 @@ export const TodoContextProvider = (props) => {
     todoDispatch: dispatchTodoAction,
     setLoadingStatus: setLoadingStatus,
     modalHandle: modalHandle,
+    setTasks: setTasks,
+    removeTask: removeTask,
+    updateTask: updateTask,
   };
   return (
     <TodoContext.Provider value={todoContext}>
