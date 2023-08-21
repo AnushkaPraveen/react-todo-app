e2f33a21c7a6a9eae2f40463b59410fd460450a0
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-08-22 01:44:59 +0530

    toast message container add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./component/home";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.css';
+import { toast, ToastContainer } from 'react-toastify';
 
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <ToastContainer/>
     </div>
   );
 }
