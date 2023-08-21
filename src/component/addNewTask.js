4783d00d43c272e4b812a5435bacb35b5155c332
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-08-21 13:26:33 +0530

    add reduce functions

--- src/component/addNewTask.js
+++ src/component/addNewTask.js
@@ -1,6 +1,7 @@
-import { useContext, useState, Alert } from "react";
+import { useContext, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import TodoContext from "../store/todoContext";
+import { CreateTask } from "../api/todoApi";
 import { Modal, Button } from "react-bootstrap";
 
 const AddNewTask = () => {
@@ -14,36 +15,31 @@ const AddNewTask = () => {
     setNewTask(event.target.value);
   };
 
-  const submit = () => {
+  const submit = async () => {
     if (!newTask) {
       setTaskValidation(true);
     } else {
       try {
+        taskContext.modalHandle();
+        taskContext.setLoadingStatus(true);
         const newTaskObject = {
           id: uuidv4(),
           name: newTask,
           isCompleted: false,
         };
+
         taskContext.todoDispatch({
           type: "ADD",
           task: newTaskObject,
         });
-        handleModal();
+
+        await CreateTask(newTaskObject);
         setTaskValidation(false);
+        taskContext.setLoadingStatus(false);
         setNewTask("");
       } catch (err) {
         alert("Something Went Wrong!");
       }
-    }
-  };
-
-  const handleModal = () => {
-    try {
-      taskContext.todoDispatch({
-        type: "MODALSHOW",
-      });
-    } catch (err) {
-      alert("Something Went Wrong!");
     }
   };
 
@@ -64,7 +60,12 @@ const AddNewTask = () => {
         {taskValidation && <p>Please enter the any task..</p>}
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={handleModal}>
+        <Button
+          variant="secondary"
+          onClick={() => {
+            taskContext.modalHandle();
+          }}
+        >
           Cancel
         </Button>
         <Button variant="primary" onClick={submit}>
