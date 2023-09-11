7235ff413ae2f0588c2d7e0f50a062216f20c5d4
AnushkaPraveen <anushkapraveen98@gmail.com>
2023-09-11 14:14:55 +0530

    add separate modal file

--- src/component/addNewTask.js
+++ src/component/addNewTask.js
@@ -2,10 +2,10 @@ import { useContext, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import TodoContext from "../store/todoContext";
 import { CreateTask } from "../api/todoApi";
-import { Modal, Button } from "react-bootstrap";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./addnewtask.css";
+import AddModal from "./addModal";
 
 const AddNewTask = () => {
   const [newTask, setNewTask] = useState();
@@ -50,38 +50,13 @@ const AddNewTask = () => {
   };
 
   return (
-    <Modal show={taskContext.modalShow}>
-      <Modal.Header>
-        <Modal.Title className="titleText">Add New Task</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <label className="titleText">Task</label>
-        <input
-          className=" form-control"
-          type="text"
-          id="task"
-          name="task"
-          placeholder="type your task..."
-          onChange={addNewTaskHandler}
-        />
-        {taskValidation && (
-          <p className="validationMessage">Please enter the any task..</p>
-        )}
-      </Modal.Body>
-      <Modal.Footer>
-        <Button
-          variant="secondary"
-          onClick={() => {
-            taskContext.modalHandle();
-          }}
-        >
-          Cancel
-        </Button>
-        <Button variant="primary" onClick={submit}>
-          Confirm
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      <AddModal
+        onSubmit={submit}
+        onInputChange={addNewTaskHandler}
+        validationState={taskValidation}
+      />
+    </>
   );
 };
 export default AddNewTask;
